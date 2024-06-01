d52650a3e79d1e7c985057ef76c8b8a4115e1d00
Faith-Nyambura <faithnyamburawamui@gmail.com>
2024-06-01 07:02:35 +0300

    first commit

--- app/src/main/java/com/example/mentorgram/Secondactivity.kt
+++ app/src/main/java/com/example/mentorgram/Secondactivity.kt
@@ -2,21 +2,17 @@ package com.example.mentorgram
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Secondactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-       val btnNext=findViewById<Button>(R.id.btnNext)
-        btnNext.setOnClickListener{
-            val intent=Intent(this,Secondactivity::class.java)
-            startActivity(intent)
-        }
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_secondactivity)
+
     }
 }
