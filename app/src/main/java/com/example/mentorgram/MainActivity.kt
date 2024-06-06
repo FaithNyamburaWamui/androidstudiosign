36a965bc4514b2114546c0d944b7f39c2d261789
Faith-Nyambura <faithnyamburawamui@gmail.com>
2024-06-06 15:56:02 +0300

    first commit

--- app/src/main/java/com/example/mentorgram/MainActivity.kt
+++ app/src/main/java/com/example/mentorgram/MainActivity.kt
@@ -2,21 +2,94 @@ package com.example.mentorgram
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
+import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.mentorgram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-       val btnNext=findViewById<Button>(R.id.btnNext)
-        btnNext.setOnClickListener{
-            val intent=Intent(this,Secondactivity::class.java)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(this, Secondactivity::class.java)
             startActivity(intent)
         }
+
+        binding.btnRegister.setOnClickListener {
+            validateRegistration()
+        }
+    }
+
+        fun validateRegistration(){
+            var formErr=false
+            clearErrors()
+            val firstName=binding.etFirstName.text.toString()
+            if(firstName.isBlank()){
+                formErr=true
+                binding.tilFirstName.error="First name is required"
+            }
+
+            val lastName=binding.etLastName.text.toString()
+            if(lastName.isBlank()){
+                formErr=true
+                binding.tilLastName.error="Last name is required"
+            }
+
+            val Email=binding.etEmail.text.toString()
+            if(firstName.isBlank()){
+                formErr=true
+                binding.tilEmail.error="Email is required"
+            }
+
+            val Codehive=binding.etCodehive.text.toString()
+            if(firstName.isBlank()){
+                formErr=true
+                binding.tilCode.error="Code is required"
+            }
+
+            val userName=binding.etUserName.text.toString()
+            if(firstName.isBlank()){
+                formErr=true
+                binding.tillUserName.error="User name is required"
+            }
+
+            val password=binding.etPassword.text.toString()
+            if(firstName.isBlank()){
+                formErr=true
+                binding.tillPassword.error="Password is required"
+            }
+
+            val confirm=binding.etConPassword.text.toString()
+            if(firstName.isBlank()){
+                formErr=true
+                binding.tillConPassword.error=" Confirm password"
+            }
+            if(password!=confirm){
+                binding.tillConPassword.error="Passwords do not match"
+            }
+            if(!formErr){
+                //proceed to registration
+        }
+        }
+
+    fun clearErrors(){
+        binding.tilFirstName.error=null
+        binding.tilLastName.error=null
+        binding.tilEmail.error=null
+        binding.tilCode.error=null
+        binding.tillUserName.error=null
+        binding.tillPassword.error=null
+        binding.tillConPassword.error=null
     }
 }
+
+    //        val textview = findViewById<TextView>(R.id.tvLogin)
+//        textview.setOnClickListener {
+//            val intent = Intent(this, Secondactivity::class.java)
+//            startActivity(intent)
+//            }
+
--- app/src/main/java/com/example/mentorgram/Secondactivity.kt
+++ app/src/main/java/com/example/mentorgram/Secondactivity.kt
@@ -2,17 +2,53 @@ package com.example.mentorgram
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Layout
+import android.view.LayoutInflater
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mentorgram.databinding.ActivityMainBinding
+import com.example.mentorgram.databinding.ActivitySecondactivityBinding
 
 class Secondactivity : AppCompatActivity() {
+    lateinit var binding: ActivitySecondactivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_secondactivity)
+        binding = ActivitySecondactivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val textView3= findViewById<TextView>(R.id.textView3)
+        textView3.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnPrev.setOnClickListener {
+            validateRegistration()
+        }
     }
+
+    fun validateRegistration() {
+        var formErr=false
+        clearError()
+        val user=binding.etUser.text.toString()
+        if(user.isBlank()){
+            formErr=true
+            binding.tillUser.error="First name is required"
+        }
+
+        val password=binding.etPassword2.text.toString()
+        if(password.isBlank()){
+            formErr=true
+            binding.tillPassword2.error="First name is required"
+        }
+    }
+
+fun clearError(){
+    binding.tillUser.error=null
+    binding.tillPassword2.error=null
+}
 }
